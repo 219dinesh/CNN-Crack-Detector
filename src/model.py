2875f502e98432e9fde8a8d6e06738232d161ce6
219dinesh <134425342+219dinesh@users.noreply.github.com>
2026-05-25 20:29:52 +0545

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,20 @@
+import torch.nn as nn
 
+class CrackDetectorCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(), nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 16 * 16, 256), nn.ReLU(), nn.Dropout(0.5),
+            nn.Linear(256, 1)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        return self.classifier(x)
